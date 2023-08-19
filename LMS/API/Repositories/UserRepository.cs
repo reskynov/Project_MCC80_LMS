622b2cfa927c73d6e883ddf05183bd460bed0c18
Lidya Ananda Arsyanti <Lidyanandaars@gmail.com>
2023-08-19 22:38:01 +0700

    Update User Validator

--- LMS/API/Repositories/UserRepository.cs
+++ LMS/API/Repositories/UserRepository.cs
@@ -9,5 +9,11 @@ namespace API.Repositories
         public UserRepository(LmsDbContext context) : base(context)
         {
         }
+        public bool IsNotExist(string value)
+        {
+            return _context.Set<User>()
+                            .SingleOrDefault(e => e.Email.Contains(value)
+                            || e.PhoneNumber.Contains(value)) is null;
+        }
     }
 }
