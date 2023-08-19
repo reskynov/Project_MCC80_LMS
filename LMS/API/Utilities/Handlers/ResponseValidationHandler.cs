481cca72fd1357512d9694ffa56545567022488b
reskynov <reskykiky2000@gmail.com>
2023-08-19 19:31:37 +0700

    add responsehandler & response validation handler

---
+++ LMS/API/Utilities/Handlers/ResponseValidationHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Utilities.Handlers
+{
+    public class ResponseValidationHandler
+    {
+        public int Code { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public string[] Errors { get; set; }
+    }
+}
