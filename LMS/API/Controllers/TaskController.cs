f185044809f78b7ff346cd6fd8a392781a95708a
Lidya Ananda Arsyanti <Lidyanandaars@gmail.com>
2023-08-19 19:58:47 +0700

    Controller Task until User

---
+++ LMS/API/Controllers/TaskController.cs
@@ -0,0 +1,154 @@
+﻿using API.DTOs.Tasks;
+using API.Services;
+using API.Utilities.Handlers;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace API.Controllers
+{
+    public class TaskController : ControllerBase
+    {
+        private readonly TaskService _taskService;
+
+        public TaskController(TaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _taskService.GetAll();
+            if (!result.Any())
+            {
+                return NotFound(new ResponseHandler<IEnumerable<TaskDto>>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "Guid is not found"
+                });
+            }
+
+            return Ok(new ResponseHandler<IEnumerable<TaskDto>>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success retrieve data",
+                Data = result
+            });
+        }
+
+        [HttpGet("{guid}")]
+        public IActionResult GetByGuid(Guid guid)
+        {
+            var result = _taskService.GetByGuid(guid);
+            if (result is null)
+            {
+                return NotFound(new ResponseHandler<TaskDto>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "Guid is not found",
+                    Data = result
+                });
+            }
+
+            return Ok(new ResponseHandler<TaskDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success retrieve data",
+                Data = result
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Insert(NewTaskDto newTaskDto)
+        {
+            var result = _taskService.Create(newTaskDto);
+            if (result is null)
+            {
+                return StatusCode(500, new ResponseHandler<NewTaskDto>
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Status = HttpStatusCode.OK.ToString(),
+                    Message = "Error retrieve from database"
+                });
+            }
+
+            return Ok(new ResponseHandler<NewTaskDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success retrieve data",
+                Data = newTaskDto
+            });
+        }
+
+        [HttpPut]
+        public IActionResult Update(TaskDto taskDto)
+        {
+            var result = _taskService.Update(taskDto);
+
+            if (result is -1)
+            {
+                return NotFound(new ResponseHandler<TaskDto>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "Guid is not found"
+                });
+            }
+
+            if (result is 0)
+            {
+                return StatusCode(500, new ResponseHandler<TaskDto>
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Status = HttpStatusCode.OK.ToString(),
+                    Message = "Error retrieve from database"
+                });
+            }
+
+            return Ok(new ResponseHandler<TaskDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success update data"
+            });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Guid guid)
+        {
+            var result = _taskService.Delete(guid);
+
+            if (result is -1)
+            {
+                return NotFound(new ResponseHandler<TaskDto>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "Guid is not found"
+                });
+            }
+
+            if (result is 0)
+            {
+                return StatusCode(500, new ResponseHandler<TaskDto>
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Status = HttpStatusCode.OK.ToString(),
+                    Message = "Error retrieve from database"
+                });
+            }
+
+            return Ok(new ResponseHandler<TaskDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success delete data"
+            });
+        }
+    }
+}
---
+++ LMS/API/Controllers/UserController.cs
@@ -0,0 +1,154 @@
+﻿using API.DTOs.Users;
+using API.Services;
+using API.Utilities.Handlers;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace API.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _userService.GetAll();
+            if (!result.Any())
+            {
+                return NotFound(new ResponseHandler<IEnumerable<UserDto>>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "Guid is not found"
+                });
+            }
+
+            return Ok(new ResponseHandler<IEnumerable<UserDto>>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success retrieve data",
+                Data = result
+            });
+        }
+
+        [HttpGet("{guid}")]
+        public IActionResult GetByGuid(Guid guid)
+        {
+            var result = _userService.GetByGuid(guid);
+            if (result is null)
+            {
+                return NotFound(new ResponseHandler<UserDto>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "Guid is not found",
+                    Data = result
+                });
+            }
+
+            return Ok(new ResponseHandler<UserDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success retrieve data",
+                Data = result
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Insert(NewUserDto newUserDto)
+        {
+            var result = _userService.Create(newUserDto);
+            if (result is null)
+            {
+                return StatusCode(500, new ResponseHandler<NewUserDto>
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Status = HttpStatusCode.OK.ToString(),
+                    Message = "Error retrieve from database"
+                });
+            }
+
+            return Ok(new ResponseHandler<NewUserDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success retrieve data",
+                Data = newUserDto
+            });
+        }
+
+        [HttpPut]
+        public IActionResult Update(UserDto userDto)
+        {
+            var result = _userService.Update(userDto);
+
+            if (result is -1)
+            {
+                return NotFound(new ResponseHandler<UserDto>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "Guid is not found"
+                });
+            }
+
+            if (result is 0)
+            {
+                return StatusCode(500, new ResponseHandler<UserDto>
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Status = HttpStatusCode.OK.ToString(),
+                    Message = "Error retrieve from database"
+                });
+            }
+
+            return Ok(new ResponseHandler<UserDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success update data"
+            });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Guid guid)
+        {
+            var result = _userService.Delete(guid);
+
+            if (result is -1)
+            {
+                return NotFound(new ResponseHandler<UserDto>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "Guid is not found"
+                });
+            }
+
+            if (result is 0)
+            {
+                return StatusCode(500, new ResponseHandler<UserDto>
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Status = HttpStatusCode.OK.ToString(),
+                    Message = "Error retrieve from database"
+                });
+            }
+
+            return Ok(new ResponseHandler<UserDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success delete data"
+            });
+        }
+    }
+}
