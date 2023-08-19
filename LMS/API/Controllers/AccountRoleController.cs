f3261f5feeb3da8413a00b965d5eb42462ae406a
reskynov <reskykiky2000@gmail.com>
2023-08-19 19:55:10 +0700

    add account, accRole, class controller + addscoped

---
+++ LMS/API/Controllers/AccountRoleController.cs
@@ -0,0 +1,155 @@
+﻿using API.DTOs.AccountRoles;
+using API.Services;
+using API.Utilities.Handlers;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/account-roles")]
+    public class AccountRoleController : ControllerBase
+    {
+        private readonly AccountRoleService _accountRoleService;
+
+        public AccountRoleController(AccountRoleService accountRoleService)
+        {
+            _accountRoleService = accountRoleService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _accountRoleService.GetAll();
+            if (!result.Any())
+            {
+                return NotFound(new ResponseHandler<AccountRoleDto>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "data not found"
+                });
+            }
+
+            return Ok(new ResponseHandler<IEnumerable<AccountRoleDto>>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success retrieve data",
+                Data = result
+            });
+        }
+
+        [HttpGet("{guid}")]
+        public IActionResult GetByGuid(Guid guid)
+        {
+            var result = _accountRoleService.GetByGuid(guid);
+            if (result is null)
+            {
+                return NotFound(new ResponseHandler<AccountRoleDto>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "guid not found"
+                });
+            }
+
+            return Ok(new ResponseHandler<AccountRoleDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success retrieve data",
+                Data = result
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Insert(NewAccountRoleDto newAccountRoleDto)
+        {
+            var result = _accountRoleService.Create(newAccountRoleDto);
+            if (result is null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHandler<AccountRoleDto>
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Status = HttpStatusCode.InternalServerError.ToString(),
+                    Message = "Internal server error"
+                });
+            }
+
+            return Ok(new ResponseHandler<AccountRoleDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success insert data",
+                Data = result
+            });
+        }
+
+        [HttpPut]
+        public IActionResult Update(AccountRoleDto accountRoleDto)
+        {
+            var result = _accountRoleService.Update(accountRoleDto);
+
+            if (result is -1)
+            {
+                return NotFound(new ResponseHandler<AccountRoleDto>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "data not found"
+                });
+            }
+
+            if (result is 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHandler<AccountRoleDto>
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Status = HttpStatusCode.InternalServerError.ToString(),
+                    Message = "Internal server error"
+                });
+            }
+
+            return Ok(new ResponseHandler<AccountRoleDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success update data"
+            });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Guid guid)
+        {
+            var result = _accountRoleService.Delete(guid);
+
+            if (result is -1)
+            {
+                return NotFound(new ResponseHandler<AccountRoleDto>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Status = HttpStatusCode.NotFound.ToString(),
+                    Message = "data not found"
+                });
+            }
+
+            if (result is 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHandler<AccountRoleDto>
+                {
+                    Code = StatusCodes.Status500InternalServerError,
+                    Status = HttpStatusCode.InternalServerError.ToString(),
+                    Message = "Internal server error"
+                });
+            }
+
+            return Ok(new ResponseHandler<AccountRoleDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Status = HttpStatusCode.OK.ToString(),
+                Message = "Success delete data"
+            });
+        }
+    }
+}
