652ec918e70afa9bb49fbeeff22bb565be328b1a
reskynov <reskykiky2000@gmail.com>
2023-08-20 13:42:01 +0700

    add validation for role guid in register

--- LMS/API/Utilities/Validations/Users/UserValidator.cs
+++ LMS/API/Utilities/Validations/Users/UserValidator.cs
@@ -32,7 +32,8 @@ namespace API.Utilities.Validations.Users
             RuleFor(u => u.PhoneNumber)
                 .MaximumLength(20)
                 .Matches(@"^\+[0-9]").WithMessage("Phone number must start with +")
-                .Must(IsDuplicationValue).WithMessage("Phone number already exist");
+                .Must(IsDuplicationValue).WithMessage("Phone number already exist")
+                .When(u => !string.IsNullOrEmpty(u.PhoneNumber)); ;
         }
 
         private bool IsDuplicationValue(string arg)
