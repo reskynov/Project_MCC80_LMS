3d4a2873a00043b11b07e5f5b80d0306b8908ce0
Lidya Ananda Arsyanti <Lidyanandaars@gmail.com>
2023-08-24 16:25:24 +0700

    Update View Lesson Teacher

--- LMS/Client/ViewModels/CombinedViews/ClassroomDetailsVM.cs
+++ LMS/Client/ViewModels/CombinedViews/ClassroomDetailsVM.cs
@@ -7,4 +7,5 @@ public class ClassroomDetailsVM
 {
     public IEnumerable<ClassroomLessonVM> ClassroomLessonViewModels { get; set; }
     public Classroom ClassroomModel { get; set; }
+    public Guid UserClassroomGuid { get; set; }
 }
